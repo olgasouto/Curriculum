8fe2700bdfdda5ef225497e22f80bb86aa977e53
olgasouto <77149445+olgasouto@users.noreply.github.com>
2021-01-30 16:25:05 +0100

    Curriculum

---
+++ index.js
@@ -0,0 +1,15 @@
+const clickBoton=document.querySelector("#boton1")
+clickBoton.addEventListener('focus', changeColorFocus1);
+function changeColorFocus1(){
+    document.getElementById("boton1").style.backgroundColor= "blue";
+};
+
+const clickBoton2=document.querySelector("#boton2")
+clickBoton2.addEventListener('focus', changeColorFocus2);
+function changeColorFocus2() {
+    document.getElementById("boton2").style.backgroundColor= "blue";
+};
+
+    
+ 
+ 
